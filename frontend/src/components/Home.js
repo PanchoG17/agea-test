4602d6d7d3b6d767ce918e6b925ed41aedb12416
Francisco Guardia <pancho.guardia.17@gmail.com>
2024-09-19 16:53:26 -0300

    feat/frontend show_winner + iconify

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 import { getComparison } from '../services/api'
 
@@ -39,70 +40,81 @@ const Home = () => {
         <div className="card mt-5">
           <form onSubmit={handleSubmit}>
               <div className="card-header bg-dark text-center text-white py-4">
-                  <h1>Ultimate Web Racer</h1>
+                  <div className="d-flex justify-content-center">
+                  <Icon icon="game-icons:bolt-bomb" className='my-auto mx-1 h1' />
+                  <h1 className="mb-0 me-4">Ultimate Web Racer</h1>
+                  </div>
                   <h2>Choose your runners:</h2>
               </div>
-              <div className="card-body bg-success text-white p-3">
+              <div className="card-body bg-success text-white px-3 py-4">
                   {error && <div className="alert alert-danger">{error}</div>}
                   <div>
                       <div className="mb-3">
-                          <label htmlFor="url_1" className="form-label">Sitio web 1:</label>
-                          <input
-                            type="url"
-                            className="form-control"
-                            id="url_1"
-                            placeholder="Ingrese la URL"
-                            value={url1}
-                            onChange={(e) => setUrl1(e.target.value)}
-                            required
-                          />
+                          <div className="d-flex">
+                            <Icon icon="emojione:racing-car" className='my-auto mx-1 h1'/>
+                            <input
+                              type="url"
+                              className="form-control"
+                              id="url_1"
+                              placeholder="Ingrese la URL 1"
+                              value={url1}
+                              onChange={(e) => setUrl1(e.target.value)}
+                              required
+                            />
+                          </div>
                       </div>
-                      <div className="mb-3">
-                          <label htmlFor="url_2" className="form-label">Sitio web 2:</label>
-                          <input
-                            type="url"
-                            className="form-control"
-                            id="url_2"
-                            placeholder="Ingrese la URL"
-                            value={url2}
-                            onChange={(e) => setUrl2(e.target.value)}
-                            required
-                          />
+                      <div>
+                          <div className="d-flex">
+                            <Icon icon="emojione:racing-car" className='my-auto mx-1 h1'/>
+                            <input
+                              type="url"
+                              className="form-control"
+                              id="url_2"
+                              placeholder="Ingrese la URL 2"
+                              value={url2}
+                              onChange={(e) => setUrl2(e.target.value)}
+                              required
+                            />
+                          </div>
                       </div>
                   </div>
               </div>
               <div className="card-footer bg-dark py-3 d-flex justify-content-center">
-
-                  {
-                  loading ? (
-                    <div className="spinner-border text-primary my-auto" role="status">
-                      <span className="visually-hidden">Loading...</span>
-                    </div>
+                  {loading ? (
+                      <div className="btn btn-light my-auto" role="status">
+                        <Icon icon="svg-spinners:eclipse-half" className='my-auto mx-1 h1' />
+                      </div>
                   )
                   :
                   (
-                    <button type='submit' className='btn btn-primary'>¡Run!</button>
+                      <button type='submit' className='btn btn-light'>
+                        <Icon icon="maki:racetrack" className='my-auto mx-1 h1' />
+                      </button>
                   )
                   }
-
               </div>
             </form>
         </div>
 
         {/* Display API response */}
         <div className="mt-5">
-          {data.length > 0 && (
+          {data.results?.length > 0 && (
             <div className="row">
-              {data.map((result, index) => (
+              {data.results.map((result, index) => (
                 <div className="col-md-6" key={index}>
                   <div className="card mb-4">
-                    <div className={`card-header text-white ${result.status == 'success' ? 'bg-success' : 'bg-danger'}`}>
-                      <h4>URL: {result.url}</h4>
-                      <p>Status: {result.status}</p>
+                    <div className={`d-flex justify-content-between card-header text-white ${result.url === data.winner ? 'bg-success' : 'bg-danger'}`}>
+                      <div className="">
+                        <h4>URL: {result.url}</h4>
+                        <p>Status: {result.status}</p>  
+                      </div>
+                      <Icon 
+                        icon={`${result.url === data.winner ? 'pepicons-pencil:thumbs-up-circle' : 'pepicons-pencil:thumbs-down-circle'}`}
+                        className='my-auto mx-1 h1'
+                      />
                     </div>
                     <div className="card-body">
-
-                      {result.status == 'success' ? (
+                      {result.status === 'success' ? (
                         <>
                           <h5 className="card-title">Metrics:</h5>
                           <p>
@@ -114,19 +126,17 @@ const Home = () => {
                             {result.metrics.time_to_interactive.numeric_value} {result.metrics.time_to_interactive.numeric_unit})
                           </p>
                         </>
-                      )
-                      :
-                      (
-                        <>
-                          <h5 className="card-title">Error message:</h5>
-                          <p>
-                            {result.message}
-                          </p>
-                        </>
-                      )}
-
-
-
+                        )
+                        :
+                        (
+                          <>
+                            <h5 className="card-title">Error message:</h5>
+                            <p>
+                              {result.message}
+                            </p>
+                          </>
+                        )
+                      }
                     </div>
                   </div>
                 </div>
